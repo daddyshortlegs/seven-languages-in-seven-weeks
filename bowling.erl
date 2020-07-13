19823b595710640e85b07d0be0a4b9d4c385207f
andys <andysmith836@gmail.com>
2020-07-13 23:09:49 +0100

    remove debug

--- bowling.erl
+++ bowling.erl
@@ -13,6 +13,9 @@ spare_test() ->
 strike_test() ->
   ?assertEqual(24, score_rolls([10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).
 
+all_spares_test() ->
+  ?assertEqual(150, score_rolls([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])).
+
 all_strikes_test() ->
   ?assertEqual(300, score_rolls([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10])).
 
@@ -23,26 +26,15 @@ score_rolls(Rolls) -> score_rolls(Rolls, 0, 0).
 score_rolls(Rolls, 10, Score) -> Score;
 
 score_rolls([Current | Rolls], Frame, Score) ->
-  io:format("Score = ~p~n", [Score]),
-
   [_ | NextFrame] = Rolls,
   [Next | _] = Rolls,
   if
     Current == 10 ->
       [Next, OneAfter | _] = Rolls,
-      io:format("Strike, next = ~p, one after ~p~n", [Next, OneAfter]),
-      io:format("Rolls = ~p~n", [Rolls]),
       score_rolls(Rolls, Frame + 1, Score + 10 + Next + OneAfter);
     Current + Next == 10 ->
       [Next, OneAfter | _] = Rolls,
-      io:format("Spare, next score = ~p~n", [OneAfter]),
-      io:format("Next Frame = ~p~n", [NextFrame]),
       score_rolls(NextFrame, Frame + 1, Score + 10 + OneAfter);
     true ->
-      io:format("Pin down, current = ~p, next = ~p~n", [Current, Next]),
       score_rolls(NextFrame, Frame + 1, Score + Current + Next)
   end.
-
-
-
-
