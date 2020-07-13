b73774d16d7db1d4e6196c846fd172c51168416a
andys <andysmith836@gmail.com>
2020-07-13 22:01:22 +0100

    Strike working (sort of)

--- bowling.erl
+++ bowling.erl
@@ -10,6 +10,9 @@ all_ones_test() ->
 spare_test() ->
   ?assertEqual(20, score_rolls([2, 8, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).
 
+strike_test() ->
+  ?assertEqual(24, score_rolls([10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).
+
 
 
 score_rolls([]) -> 0;
@@ -21,6 +24,11 @@ score_rolls([Current | Rolls]) ->
                  end,
   io:format("Current roll = ~p, Next roll = ~p~n", [Current, Next]),
   if
+    Current == 10 ->
+      NextScore = lists:nth(1, Rolls),
+      OneAfter = lists:nth(2, Rolls),
+      10 + NextScore + OneAfter + score_rolls(Rolls);
+
     Current + Next == 10 ->
       NextScore = lists:nth(2, Rolls),
       io:format("Spare, next score = ~p~n", [NextScore]),
